1106b10474fb3fb3175d55c8b7e27018ce6ed2b8
ashish <1508ramashish@gmail.com>
2024-02-27 17:35:33 +0530

    vectorupdata

---
+++ JS/im.jsx
@@ -0,0 +1,4 @@
+// PRINTING ARRAY OF OBEJECT  similar to for(auto i:araay)
+{arrayname.map((arrayitem, index) => (
+    <li key={index}>{arrayitem.name}</li>
+  ))}
---
+++ map/multimap.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+// deaclearation of multimap
+// mutlimap help to create duplicate key also
+multimap<int,int> m;
+int key,value;
+// insertion in multimap 
+m.insert({key,value});
+}
